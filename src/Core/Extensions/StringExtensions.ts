49580e84357a0ccdc0fe222035a251042bdb771e
Дмитрий Сердюков <dimaserd84@gmail.com>
2019-12-27 14:26:08 +0300

    FIxes

--- src/Core/Extensions/StringExtensions.ts
+++ src/Core/Extensions/StringExtensions.ts
@@ -1,9 +1,9 @@
-﻿interface String {
-    format(...params: String[]): string;
-}
+﻿//interface String {
+//    format(...params: String[]): string;
+//}
 
-String.prototype.format = function (...params: String[]) {
-    return this.replace(/{(\d+)}/g, function (match, number) {
-        return typeof params[number] != 'undefined' ? params[number] : match;
-    });
-};
+//String.prototype.format = function (...params: String[]) {
+//    return this.replace(/{(\d+)}/g, function (match, number) {
+//        return typeof params[number] != 'undefined' ? params[number] : match;
+//    });
+//};
