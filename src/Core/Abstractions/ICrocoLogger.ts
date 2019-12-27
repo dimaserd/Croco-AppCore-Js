fe6351f019297caf2d112292fb3b82adcba6bf73
Дмитрий Сердюков <dimaserd84@gmail.com>
2019-12-27 15:46:20 +0300

    Fixes

--- src/Core/Abstractions/ICrocoLogger.ts
+++ src/Core/Abstractions/ICrocoLogger.ts
@@ -1,4 +1,18 @@
 ﻿export interface ICrocoLogger {
+    /**
+     * Залоггировать исключение
+     * @param exceptionText
+     * @param exceptionDescription
+     * @param link
+     */
     LogException(exceptionText: string, exceptionDescription: string, link: string): void;
+
+    /**
+     * Залоггировать действие
+     * @param message
+     * @param description
+     * @param eventId
+     * @param parametersJson
+     */
     LogAction(message: string, description: string, eventId: string, parametersJson: string): void;
 }
--- src/Core/Extensions/StringExtensions.ts
+++
@@ -1,9 +0,0 @@
-﻿//interface String {
-//    format(...params: String[]): string;
-//}
-
-//String.prototype.format = function (...params: String[]) {
-//    return this.replace(/{(\d+)}/g, function (match, number) {
-//        return typeof params[number] != 'undefined' ? params[number] : match;
-//    });
-//};
